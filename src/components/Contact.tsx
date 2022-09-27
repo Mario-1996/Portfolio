021513ec3976f451b9ec481b8247e4c916f37d5a
Mario-1996 <mario1996prohman@gmail.com>
2022-09-27 16:32:08 -0300

    contact colors

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,14 +11,14 @@ function Contact() {
                             Contatos
                         </h1>
                         
-                        <div className="flex items-center mt-8 text-white dark:text-gray-400">
+                        <div className="flex items-center mt-8 text-white dark:text-white">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                             </svg>
                             <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                Acme Inc, Street, State,
-                                Postal Code
+                                Umuarama-Pr 
+                                CEP: 87502-140
                             </div>
                         </div>
                         <div className="flex items-center mt-4 text-white dark:text-gray-400">
